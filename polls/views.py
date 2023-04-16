3ede16e08e89d8d770a187cbc5012959cc33e05c
Gabriela Andrade <gabrielav.sudo@gmail.com>
2023-04-15 21:20:13 -0300

    Update view.py

--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Always for luck. Hello, world!!")
+    return HttpResponse("Always for luck. Hello, world!! OpenShift - RedHat")
